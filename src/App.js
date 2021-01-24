4e04e7c373a3272b8502cb78146607510fe21c02
yuesth <mymac@mymacs-MacBook-Pro-2.local>
2021-01-24 21:05:34 +0700

    endpoint, improve

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import GetDonation from './pages/tabel/getDonation'
 import GetCampaign from './pages/tabel/getCampaign'
+import DetailDonation from './pages/tabel/detaildonation'
+import DetailCampaign from './pages/tabel/detailcampaign'
 import QrCodewa from './pages/qrcodewa'
 import Login from './pages/login'
 import Dashboard from './pages/dashboard'
@@ -14,6 +16,8 @@ function App() {
         <Switch>
           <Route path="/" exact component={Login}></Route>
           <Route path="/tabel/getDonation/:id" exact component={GetDonation}></Route>
+          <Route path="/tabel/getDonation/detail/:id" exact component={DetailDonation}></Route>
+          <Route path="/tabel/getCampaign/detail/:id" exact component={DetailCampaign}></Route>
           <Route path="/tabel/getCampaign" exact component={GetCampaign}></Route>
           <Route path="/dashboard" exact component={Dashboard}></Route>
           <Route path="/qr-code-wa" exact component={QrCodewa}></Route>
---
+++ src/pages/tabel/detaildonation.js
@@ -0,0 +1,69 @@
+import { Link, useHistory } from 'react-router-dom'
+import Layout from '../../layout'
+import Modal from "react-bootstrap/Modal"
+import { useState, useCallback } from 'react'
+
+function DetailDonation(props) {
+    const hist = useHistory()
+    const [open, setOpen] = useState(false)
+    if (!props.location.state) {
+        hist.push('/tabel/getDonation/0')
+    }
+    const data = props.location.state.data
+    const handleOpen = () => setOpen(prev => !prev)
+    const handleDeleteDonation = useCallback((namaDonatur) => {
+        const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6ODAwMFwvbG9naW4iLCJpYXQiOjE2MTA0MjgzNzgsImV4cCI6MTYxMDQzMTk3OCwibmJmIjoxNjEwNDI4Mzc4LCJqdGkiOiJWSTFEZkVORjZWc3luNHB2Iiwic3ViIjoxMDAxLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.awgkdKJarKGTxP_0HIldNI7CnG_xtJoxnzhALuFGIPc'
+        // fetch(`https://donasi.aqlpeduli.or.id/deleteCampaign?token=${token}&id=${idcamp}`, {
+        //     method: "DELETE",
+        // }).then(res => {
+        //     console.log(res)
+        //     window.location.href = "http://localhost:3000/tabel/getCampaign"
+        // })
+    }, [])
+    return (
+        <Layout>
+            <div className="row w-100 mb-4">
+                <div className="col-2 w-100">
+                    <Link to="/tabel/getDonation/0" style={{ textDecoration: `none` }}>
+                        <i className="fa fa-chevron-left fa--2x"></i> Back
+                    </Link>
+                    <h3>Detail Donation</h3>
+                    <hr className="w-100" />
+                </div>
+            </div>
+            {/* <form> */}
+            <div className="mb-3">
+                <label htmlFor="recipient-name" className="col-form-label">Nama:</label>
+                <input type="text" className="form-control" id="recipient-name" defaultValue={data.nama} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="message-text" className="col-form-label">Email:</label>
+                <input type="text" className="form-control" id="message-text" defaultValue={data.email} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="message-text" className="col-form-label">No handphone:</label>
+                <input type="text" className="form-control" id="message-text" defaultValue={data.nohp} />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="message-text" className="col-form-label">Amount:</label>
+                <input type="number" className="form-control" id="message-text" defaultValue={data.amount} />
+            </div>
+            <button type="button" className="btn btn-warning w-25" style={{ marginRight: `1rem` }}>Edit</button>
+            <button type="button" className="btn btn-danger" onClick={handleOpen} data-bs-toggle="modal" data-bs-target="#hapusModal">Hapus</button>
+            {/* </form> */}
+            <Modal show={open} onHide={handleOpen}>
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Konfirmasi hapus</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleOpen} />
+                </div>
+                <Modal.Body>Yakin ingin menghapus data donation dari Sdr/i. <strong>{data.nama}</strong>?</Modal.Body>
+                <Modal.Footer>
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={handleOpen}>Tutup</button>
+                    <button type="button" className="btn btn-danger" onClick={() => handleDeleteDonation(data.nama)}>Hapus</button>
+                </Modal.Footer>
+            </Modal>
+        </Layout>
+    )
+}
+
+export default DetailDonation
